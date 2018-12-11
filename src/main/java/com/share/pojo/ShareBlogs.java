125f1ee1fe5277ae335df18ea92a11a5f80a1526
博博 <mhb0409@qq.com>
2018-12-11 18:30:44 +0800

    进行了shareblogs的优化

--- src/main/java/com/share/pojo/ShareBlogs.java
+++ src/main/java/com/share/pojo/ShareBlogs.java
@@ -4,6 +4,10 @@ import com.baomidou.mybatisplus.annotation.IdType;
 import com.baomidou.mybatisplus.extension.activerecord.Model;
 import java.time.LocalDateTime;
 import com.baomidou.mybatisplus.annotation.TableId;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
 import java.io.Serializable;
 
 /**
@@ -14,6 +18,9 @@ import java.io.Serializable;
  * @author Bean
  * @since 2018-12-11
  */
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class ShareBlogs extends Model<ShareBlogs> {
 
     private static final long serialVersionUID = 1L;
